26a93b5234c274fa8f1e4099559c32262aa1ba0b
Sahrane_Guassemi <sahranedev@gmail.com>
2023-01-12 11:21:27 +0100

    login page fix some details

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,19 +2,16 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import logo from "../../images/Logo-Sapeurs-Pompiers.jpg";
 
 export default function Header() {
   const navigate = useNavigate();
   return (
-    <div className="h-20 flex justify-between bg-slate-200">
-      <div className="w-20 mt-2 cursor-pointer">
-        <img
-          onClick={() => navigate("/")}
-          className="h-10 w-18"
-          src={logo}
-          alt="logo"
-        />
+    <div className="grid grid-cols-12 gap-4 h-20 bg-blue-900">
+     
+      <div className="col-start-5 flex items-center justify-center col-end-9">
+        <h1 className=" text-white text-center text-2xl font-bold">
+          Pompiers de Lyon
+        </h1>
       </div>
     </div>
   );
