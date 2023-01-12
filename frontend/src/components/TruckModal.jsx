0aec69e9f4df24387235b79fc6c63649bccb74bc
Dreyrk <lucas.rondepierre123@gmail.com>
2023-01-12 12:36:54 +0100

    ppapka

--- frontend/src/components/TruckModal.jsx
+++ frontend/src/components/TruckModal.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import TruckInfos from "./TruckInfos";
 
 export default function TruckModal() {
-  const [description, setDescription] = useState("");
+  const [description /* setDescription */] = useState("");
   return (
     <div className="m-2 p-2 h-[75vh] w-[80vw] flex flex-wrap justify-between border-4 rounded-2xl border-gray-300 border-solid">
       <TruckInfos description={description} />
